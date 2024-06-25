792eb3e2ed439505835f61333273ab48d4193636
attila <apatel@aviatrix.com>
2024-06-25 10:33:54 +0100

    rectified aws access keyid

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, Everyone and Atul!" > index.html
+              echo "Hello, Everyone and Atul, typo AWS access keyid!" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
