4a4624c9b2e4243e4226f19c380e02452ccf87da
attila <apatel@aviatrix.com>
2024-06-25 10:55:42 +0100

    push to deploy

--- main.tf
+++ main.tf
@@ -36,7 +36,7 @@ resource "aws_instance" "web" {
 
   user_data = <<-EOF
               #!/bin/bash
-              echo "Hello, Everyone and Atul, typo AWS access keyid!" > index.html
+              echo "Hello, Everyone and Atul, typo AWS access keyid, - 'push' to run apply!" > index.html
               nohup busybox httpd -f -p 8080 &
               EOF
 }
